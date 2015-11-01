1dba6642ff81479c108cd45a1f73a5c84d860dcf
dmitry <dooman87@gmail.com>
2015-11-01 20:51:00 +1100

    Added like functionality.

--- like.js
+++ like.js
@@ -1,12 +1,113 @@
-window.setTimeout(function() {
-  var pics = document.getElementsByClassName("-cx-PRIVATE-Photo__clickShield");
-  chrome.runtime.sendMessage({type: "FROM_PAGE", text: "Found " + pics.length + " images"});
-}, 1000);
+
+var Insta = function() {
+    var IMG_SEL = ".-cx-PRIVATE-PostsGrid__item .-cx-PRIVATE-Photo__clickShield";
+    var MORE_LINK_SEL = ".-cx-PRIVATE-AutoloadingPostsGrid__moreLink";
+    var IMG_CLOSE_SEL = ".-cx-PRIVATE-Modal__closeButton";
+    var LIKE_SEL = ".-cx-PRIVATE-PostInfo__likeButton.coreSpriteHeartOpen";
+
+    var imagesQueue = [];
+    var stop = false;
+
+    /**
+     * Observing document to find new images that is loading
+     * when "Load more" button pressed or the page was scrolled 
+     * down
+     */
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(m) {
+            if (m.addedNodes) {
+                for (var i = 0; i < m.addedNodes.length; i++) {
+                    var el = $(m.addedNodes.item(i));
+                    el.find(IMG_SEL).each(addImages);
+                }
+            }
+        });
+    });
+
+    function addImages(idx, img) {
+        imagesQueue.push(img);
+    }
+
+    // Returns a random integer between min (included) and max (included)
+    // Using Math.round() will give you a non-uniform distribution!
+    function getRandomIntInclusive(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    /**
+     * Pull an image from a queue and like it.
+     * Schedule the next call in period from
+     * 2 - 5 seconds. Also, stops execution if
+     * "stop" flag is set.
+     */
+    function likeImg() {
+        if (stop) {
+            return;
+        }
+
+        var img = $(imagesQueue.shift());
+
+        if (img) {
+            $(window).scrollTop(img.offset().top);
+            img.click();
+
+            window.setTimeout(function() {
+                var likeButton = $(LIKE_SEL);
+                if (likeButton.length) {
+                    likeButton[0].click();
+                }
+                $(IMG_CLOSE_SEL).click();
+                likeImg();
+            }, getRandomIntInclusive(3000, 6000));
+        }
+    }
+
+    var doStart = function() {
+        var config = {childList: true, subtree: true};
+        observer.observe(document, config);
+
+        imagesQueue = [];
+        $(IMG_SEL).each(addImages);
+
+        var moreImagesEl = $(MORE_LINK_SEL);
+        if (moreImagesEl) {
+            moreImagesEl[0].click();
+        }
+        likeImg();
+    }
+
+    var doStop = function() {
+        stop = true;
+        observer.disconnect();
+    }
+
+    var info = function info() {
+        return {
+            imagesCount: $(IMG_SEL).length,
+            login: $("[data-reactid*='$profileLink']").text()
+        }
+    }
+
+    return {
+        doStart: doStart,
+        doStop:  doStop,
+        info:    info
+    }
+
+}();
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.type == "FROM_EXTENSION" && request.text == "START") {
-        alert("START LIKE");
-        $('.-cx-PRIVATE-AutoloadingPostsGrid__moreLink')[0].click();
-    }
+
+      if (request.type == "FROM_EXTENSION") {
+          if (request.text == "START") {
+              Insta.doStart();
+          }
+          if (request.text == "STOP") {
+              Insta.doStop();
+          }
+          if (request.text == "INFO") {
+              chrome.runtime.sendMessage({type: "FROM_PAGE", data: Insta.info()});
+          }
+      }
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,35 @@
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+    document.getElementById('status').textContent = statusText;
+}
+
+function sendMessage(text) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "FROM_EXTENSION", text: text})
+    });
 }
 
 $(document).ready(function() {
-    renderStatus(chrome.extension.getBackgroundPage().imagesCount);
+    chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+        if (msg.type == "FROM_PAGE") {
+            if (msg.data.login && msg.data.imagesCount) {
+                $(".buttons").show();
+                renderStatus("@" + msg.data.login + ", found " + msg.data.imagesCount + " images");
+            } else {
+                $(".buttons").hide();
+                renderStatus("Please login and jump to the page with images");
+            }
+        }
+    });
+
+
+    sendMessage("INFO");
 
     $('.js-start').on('click', function() {
-      window.alert("Start");
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: "FROM_EXTENSION", text: "START"})
-      });
+        sendMessage("START");
+    });
+
+    $('.js-stop').on('click', function() {
+        sendMessage("STOP");
     });
 });
 
